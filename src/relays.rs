995e75adf81cf7cc1fc855e518de8c33ad8102eb
Ben <feba23@live.com>
2023-12-24 16:44:37 -0600

    Initial commit

---
+++ src/relays.rs
@@ -0,0 +1,143 @@
+use serde_json::{Value};
+
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::{vec};
+use std::convert::{TryFrom};
+use serde_json::json;
+
+pub struct KasaPlug {
+    ip: String,
+    name: String,
+    pub(crate) status: bool,
+    tags: Vec<String>,
+    room: String,
+}
+
+impl KasaPlug {
+    fn decrypt(string: Vec<u8>) -> String {
+        let key: u8 = 171;
+        let mut result = String::new();
+        let mut prev = key;
+        for i in string {
+            let a = prev ^ i;
+            prev = i;
+            result.push(a as char);
+        }
+        result
+    }
+
+    fn encrypt(string: String) -> Vec<u8> {
+        let mut key: u8 = 171;
+        let mut result: Vec<u8> = vec![];
+        result.extend_from_slice(&(string.len() as u32).to_be_bytes());
+        for i in string.bytes() {
+            let a = key ^ i;
+            key = a;
+
+            result.push(a);
+        }
+        return result;
+    }
+
+    pub fn new(ip: String, name: String, room: String) -> Self {
+        let mut tags: Vec<String> = Vec::new();
+        let name = if name.is_empty() {
+            tags.push(format!("Plug {}", ip));
+            format!("Plug {}", ip)
+        } else {
+            name
+        };
+        let mut plug = KasaPlug {
+            ip,
+            name,
+            status: false,
+            tags,
+            room,
+        };
+        plug.status = KasaPlug::get_status(&mut plug);
+        plug
+    }
+
+
+    pub fn send(&self, cmd: String) -> Value {
+        const PORT: u16 = 9999;
+        let timeout = 10;
+        let mut stream = TcpStream::connect((self.ip.to_string(), PORT)).unwrap();
+        stream.set_read_timeout(Some(std::time::Duration::from_secs(timeout))).unwrap();
+        let encrypted = KasaPlug::encrypt(cmd);
+        stream.write_all(&encrypted).unwrap();
+        let mut data = vec![0; 4096];
+        stream.read(&mut data).unwrap();
+
+        let a_ref: &[u8] = &data[..4];
+        let b = i32::from_be_bytes(<[u8; 4]>::try_from(a_ref).expect("Oops, I did it again..."));
+
+        let end_pos: i32 = b + 4 as i32;
+
+        let decrypted = KasaPlug::decrypt(data[4..end_pos as usize].to_vec());
+        let json_data: Value = serde_json::from_str(&decrypted.as_str()).unwrap();
+        return json_data;
+    }
+
+    pub fn get_info(&self) -> Value {
+        let cmd = json!({"system": {"get_sysinfo": {}}});
+        self.send(cmd.to_string())["system"]["get_sysinfo"].clone()
+    }
+
+    pub fn wlan_scan(&self) -> Value {
+        let cmd = json!({"netif": {"get_scaninfo": {"refresh": 0}}});
+        self.send(cmd.to_string())
+    }
+
+
+    pub(crate) fn connected(&mut self) -> bool {
+        self.get_status()
+    }
+
+    pub fn meta(&mut self) -> Value {
+        return json!({
+            "type": "Kasa Plug",
+            "ip": &self.ip.to_string(),
+            "room": &self.room.to_string(),
+            "status": &self.status,
+            "tags": &self.tags,
+        });
+    }
+
+    pub fn get_status(&mut self) -> bool {
+        let cmd = json!({"system": {"get_sysinfo": {}}});
+        let relay_state = self.send(cmd.to_string())["system"]["get_sysinfo"]["relay_state"].as_u64().unwrap_or(0) == 1;
+        self.status = relay_state;
+        relay_state
+    }
+
+    pub fn turn_off(&mut self) {
+        self.status = false;
+        let cmd = json!({"system": {"set_relay_state": {"state": 0}}});
+        let _ = self.send(cmd.to_string())["system"].clone();
+        self.status = false;
+    }
+
+    pub fn turn_on(&mut self) {
+        self.status = true;
+        let cmd = json!({"system": {"set_relay_state": {"state": 1}}});
+        let _ = self.send(cmd.to_string())["system"].clone();
+        self.status = true;
+    }
+
+    pub fn switch(&mut self) {
+        if self.status {
+            self.turn_off()
+        } else {
+            self.turn_on()
+        }
+    }
+}
+
+impl std::fmt::Display for KasaPlug {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
