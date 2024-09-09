56de15d7f49032b51e72c87aad9a8c23fec4cfa6
Ben <feba23@live.com>
2024-09-09 12:20:14 -0500

    NonMutexRelaysAndPrests
    * Removing unnecessary mutex from relays and presets
    
    * Fixing mongodb refresh bug
    
    * Fixing mongodb refresh bug

--- src/models/presets.rs
+++ src/models/presets.rs
@@ -4,8 +4,6 @@ use serde_json::Value;
 use std::collections::HashMap;
 use std::io::Error;
 
-use std::sync::Mutex;
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Preset {
     pub(crate) name: String,
@@ -15,9 +13,8 @@ pub struct Preset {
 
 pub(crate) fn set_preset(
     preset: &Preset,
-    relays: &Mutex<HashMap<String, RelayType>>,
+    relays: &mut HashMap<String, RelayType>,
 ) -> Result<bool, Error> {
-    let mut relays = relays.lock().expect("Error getting global RELAYS");
     for (relay_name, relay) in relays.iter_mut() {
         match preset.relays.get_key_value(relay_name) {
             Some((_, &value)) => {
@@ -48,15 +45,8 @@ pub(crate) fn set_preset(
     Ok(true)
 }
 
-pub(crate) fn get_preset_names(
-    presets: &Mutex<HashMap<String, Preset>>,
-) -> Result<Vec<Value>, Error> {
-    let mut keys: Vec<String> = presets
-        .lock()
-        .unwrap()
-        .keys()
-        .map(|key| key.clone().to_string())
-        .collect();
+pub(crate) fn get_preset_names(presets: &HashMap<String, Preset>) -> Result<Vec<Value>, Error> {
+    let mut keys: Vec<String> = presets.keys().map(|key| key.clone().to_string()).collect();
     keys.sort();
     Ok(keys.into_iter().map(|s| Value::from(s)).collect())
 }
